a7190d52c765085104cd237d3c674cc23dd56784
Erik Polzin <eriktpol@gmail.com>
2023-05-14 12:44:48 +0200

    Test in one room only & increase epochs

--- ExecutionSkeleton.py
+++ ExecutionSkeleton.py
@@ -6,14 +6,6 @@ from typing import Dict, Iterable, Optional, Tuple
 import numpy as np
 from FourRooms import FourRooms
 
-# For 20 epochs
-# Scenario: (LearningRate, DiscountRate)
-DEFAULT_PARAMS = {
-    "simple": (0.15, 0.2),
-    "multi": (0.15, 0.5),
-    "rgb": (0.45, 0.65)
-}
-
 
 class FourRoomsAgent(object):
     """A RL Agent designed to find packages distributed over a map with boundaries."""
@@ -30,13 +22,10 @@ class FourRoomsAgent(object):
             """Hash for state lookup."""
             return hash((self.x, self.y, self.numPackagesRemaining))
 
-    def __init__(self,
-                 env: FourRooms,
-                 learning_rate: Optional[float] = None,
-                 discount_rate: Optional[float] = None):
+    def __init__(self, env: FourRooms, learning_rate: float, discount_rate: float):
         self.env = env
-        self.learning_rate = learning_rate or DEFAULT_PARAMS[env.scenario][0]
-        self.discount_rate = discount_rate or DEFAULT_PARAMS[env.scenario][1]
+        self.learning_rate = learning_rate
+        self.discount_rate = discount_rate
         self.V: Dict[FourRoomsAgent.State, Dict[int, float]] = {}
         for s in self.allPossibleStates():
             # Initialize random values for each state-action pair
@@ -108,54 +97,71 @@ class FourRoomsAgent(object):
         return FourRoomsAgent.State(x=x, y=y, numPackagesRemaining=n)
 
 
-def test(scenario: str, **kwargs) -> None:
+def test(scenario: str, stochastic: bool, epochs: int) -> None:
     """Test a range of parameters (LR, DR) to find the best combination."""
     best = -float("inf")
     bestparams = (-1, -1)
-    epochs = kwargs.get("epochs", None)
+    # Test in the same room
+    room = FourRooms(scenario, stochastic=stochastic)
     print(f"Testing parameters with {epochs} epochs")
     for lr in np.arange(0.05, 0.95, 0.05):
-        kwargs["learning_rate"] = lr
         for dr in np.arange(0.05, 0.95, 0.05):
-            kwargs["discount_rate"] = dr
-            fitness = run(scenario, **kwargs)[1]
+            fitness = run(scenario,
+                          stochastic=stochastic,
+                          epochs=epochs,
+                          learning_rate=lr,
+                          discount_rate=dr,
+                          room=room)[1]
             if fitness > best:
                 print(f"LR: {lr:.3f}, DR: {dr:.3f} ({fitness:.0f}%)")
                 best = fitness
                 bestparams = (lr, dr)
-    print(f"Best LR: {bestparams[0]}, DR: {bestparams[1]}")
+            room.newEpoch()
+    print(f"Best LR: {bestparams[0]:.3f}, DR: {bestparams[1]:.3f}")
 
 
 def run(scenario: str,
-        stochastic: bool = False,
-        epochs: int = 20,
-        learning_rate: Optional[float] = None,
-        discount_rate: Optional[float] = None,
+        stochastic: bool,
+        epochs: int,
+        learning_rate: float,
+        discount_rate: float,
         feedback: bool = False,
-        max_actions: int = 10000) -> Tuple[FourRooms, float]:
+        room: Optional[FourRooms] = None,
+        max_loops: int = 10000) -> Tuple[FourRooms, float]:
     """Run a given scenario and return the average moves before picking up all packages."""
+    if room is None:
+        # Create FourRooms Object
+        room = FourRooms(scenario, stochastic=stochastic)
+    agent = FourRoomsAgent(room, learning_rate, discount_rate)
+    N = room.getPackagesRemaining()
+    # The maximum number of actions before collecting a package in the opposing corner
+    max_actions = 13*2*N
+    avg_score = 0.0
+    feedback_mod = epochs // 10
 
-    # Create FourRooms Object
-    fourRoomsObj = FourRooms(scenario, stochastic=stochastic)
-    agent = FourRoomsAgent(fourRoomsObj, learning_rate, discount_rate)
-    fitness = 0.0
+    if feedback:
+        print("Running with learning_rate={:.3f}, discount_rate={:.3f}".format(
+            agent.learning_rate, agent.discount_rate))
 
     for epoch in range(epochs):
         i = 0
-        fourRoomsObj.newEpoch()
-        N = fourRoomsObj.getPackagesRemaining()
-        # Cap the number of moves at max_actions, in case of infinite loops
-        while not fourRoomsObj.isTerminal() and i < max_actions:
+        room.newEpoch()
+        # Cap the number of moves at max_loops, in case of infinite loops
+        while not room.isTerminal() and i < max_loops:
             prevState = agent.state()
             nextAction = agent.getBestAction()
             reward = agent.takeAction(nextAction)
             agent.updateV(prevState, nextAction, reward)
             i += 1
-        n = N - fourRoomsObj.getPackagesRemaining()
-        fitness += 0.2*(1-i/max_actions)+0.8*(n/N)*100
-        if feedback:
-            print(f"Found {n} packages in {i} moves")
-    return fourRoomsObj, fitness/epochs
+        n = N - room.getPackagesRemaining()
+        pathScore = min(1, max(0, (1-i/max_actions)))
+        packageScore = n/N
+        score = (0.5*pathScore+0.5*packageScore)*100
+        if feedback and (epoch % feedback_mod == 0):
+            print(f"{epoch:<03}: Found {n} packages in {i} moves ({score:.0f}% score)")
+        avg_score += score / epochs
+    # Return room and score. Here efficient paths and package scores are weighted evenly
+    return room, avg_score
 
 
 def main():
@@ -164,24 +170,20 @@ def main():
         description='Trains the FourRooms Agent in a given scenario')
     parser.add_argument('scenario')
     parser.add_argument('-stochastic', action='store_true')
-    parser.add_argument('-learning-rate', default=None, type=float)
-    parser.add_argument('-discount-rate', default=None, type=float)
-    parser.add_argument('-epochs', default=20, type=int)
+    parser.add_argument('-learning-rate', default=0.6, type=float)
+    parser.add_argument('-discount-rate', default=0.3, type=float)
+    parser.add_argument('-epochs', default=300, type=int)
     parser.add_argument('-test', action='store_true')
     parser.add_argument('-save', default=None, type=str)
 
     args = parser.parse_args()
-    kwargs = {
-        "stochastic": args.stochastic,
-        "epochs": args.epochs,
-        "learning_rate": args.learning_rate,
-        "discount_rate": args.discount_rate
-    }
     if args.test:
-        test(args.scenario, **kwargs)
+        test(args.scenario, args.stochastic, args.epochs)
     else:
         startTime = time.time()
-        fourRoomsObj, fitness = run(args.scenario, feedback=True, **kwargs)
+        fourRoomsObj, fitness = run(
+            args.scenario, args.stochastic, args.epochs,
+            args.learning_rate, args.discount_rate, feedback=True)
         elapsedTime = time.time() - startTime
         print(f"Ran with average fitness: {fitness:.0f}%")
         print(f"Execution time: {elapsedTime*1000:.0f}ms")
